b34ef809f7b65941296521d088cfdd22a3b02248
Luciano Rodrigues <lucianoxjktop@gmail.com>
2024-12-05 20:07:51 -0300

    starting the second part of the 'main' element

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,8 @@ export default function Home() {
 
   return (
     <div>
-      <nav className="flex justify-around items-center bg-zinc-100 p-8 border">
-        {/*logo*/}
+      <nav className="fixed top-0 left-0 w-full flex justify-around items-center bg-zinc-100 p-4 border z-10">
+        {/* Logo */}
         <div>
           <h1 className="font-semibold text-xl">Luciano Rodrigues</h1>
         </div>
@@ -24,7 +24,7 @@ export default function Home() {
       </nav>
 
       {isMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="fixed z-10 inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-md shadow-lg relative w-3/4 max-w-md">
             <button className="absolute top-2 right-2 text-gray-600" onClick={toggleMenu}>
               &#10005;
@@ -55,9 +55,9 @@ export default function Home() {
         </div>
       )}
 
-      <main className="flex flex-col justify-center gap-12">
-        {/* summary about */}
-        <div className="flex flex-col justify-center items-center text-center gap-10 mt-10">
+      <main className="mt-32 flex flex-col justify-center gap-12">
+        {/* Summary about */}
+        <div className="flex flex-col justify-center items-center text-center gap-10">
           <h1 className="text-6xl font-bold">Web Developer</h1>
           <p className="w-72">
             Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus sit netus nascetur morbi quam nulla primis.
@@ -66,14 +66,23 @@ export default function Home() {
           </p>
         </div>
         <div className="flex items-center justify-center">
-          <div className="relative overflow-hidden rounded-full w-48 h-48">
+          <div className="relative overflow-hidden rounded-t-full w-48 h-48 shadow-[10px_12px_0px_rgba(183,48,7,1)] border-2 border-zinc-950">
             <Image
               src="/photo-me.JPEG"
               alt="Luciano Photo"
               layout="fill"
               objectFit="cover"
-              className="rounded-full transition-transform duration-500 group-hover:scale-110"
+              className="rounded-t-full transition-transform duration-500 group-hover:scale-110"
             />
+          </div>
+        </div>
+        <div className="bg-zinc-950 pt-24 pr-8 pb-24 pl-8">
+          <div className="flex flex-col gap-4 border-dashed-custom border-dashed p-6">
+            <h1 className="text-xl text-zinc-100 font-semibold">Ferramentas que uso e estou aprendendo</h1>
+            <p className="text-zinc-300">
+              The most effective way to stay organized and productive in my work is by utilizing these four essential
+              tools, whether I'm working alone or collaborating with a team.
+            </p>
           </div>
         </div>
       </main>
