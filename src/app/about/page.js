4ed1692d94265378f8b548674efce61d1a93bc27
sushinectar <lucianoxjktop@gmail.com>
2025-03-02 02:29:43 -0300

    update email

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -41,7 +41,7 @@ export default function About() {
         </div>
         <div className="chat hidden mt-8">
           <a
-            href="mailto:sushi@sushi.dev"
+            href="mailto:contato.sushinectar@gmail.com"
             className="flex justify-center items-center gap-4"
           >
             <button className="chat-btn bg-custom p-2 rounded-full border border-zinc-900">
